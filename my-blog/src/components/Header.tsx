b41560ba32b8b74407d82e11d88e72167057d2fb
kemme77 <linus.kemmeter@gmx.de>
2026-04-06 13:43:42 +0200

    .

--- my-blog/src/components/Header.tsx
+++ my-blog/src/components/Header.tsx
@@ -6,11 +6,11 @@ import Container from './Container'
 const Header = () => {
   return (
     <header className="w-full flex items-center space-x-4">
-      <h1 className="text-2xl font-bold text-left mb-0">Welcome to My Site</h1>
+      <h1 className="text-2xl font-bold text-left mb-0">Welcome to my travel blog</h1>
         <Container className="bg-red-300 max-w-lg">
           {/* Logo */}
           <Image src={testimage} alt="TestImage" height={50} width={50} />
-          {/* Navigation */}
+          {/* Navigation Bar */}
           {/* Theme Switcher */}
           {/* Input */}
         </Container>
